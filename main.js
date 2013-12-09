7b6c8133aecf2c3d542f7cedce9d7dc7513cc7ae
imilka <pkzl22@gmail.com>
2013-12-09 11:34:18 +0400

    Minor template and logic fixes

--- main.js
+++ main.js
@@ -767,6 +767,753 @@ function TabManagerCtrl($scope, ajaxService) {
       ]
     }
   ];
+  $scope.posDataDep = [
+    {
+      "partOfSpeech": "S",
+      "text": "существительное",
+      "properties": [
+        {
+          "dbfield": "animacy",
+          "text": "одушевленность",
+          "values": [
+            {
+              "ident": "ОД",
+              "text": "одушевленное"
+            },
+            {
+              "ident": "НЕОД",
+              "text": "неодушевленное"
+            }
+          ]
+        },
+        {
+          "dbfield": "gender",
+          "text": "род",
+          "values": [
+            {
+              "ident": "МУЖ",
+              "text": "мужской",
+            },
+            {
+              "ident": "ЖЕН",
+              "text": "женский"
+            },
+            {
+              "ident": "СРЕД",
+              "text": "средний"
+            }
+          ]
+        },
+        {
+          "dbfield": "count",
+          "text": "число",
+          "values": [
+            {
+              "ident": "ЕД",
+              "text": "единственное"
+            },
+            {
+              "ident": "МН",
+              "text": "множественное"
+            }
+          ]
+        },
+        {
+          "dbfield": "case",
+          "text": "падеж",
+          "values": [
+            {
+              "ident": "ИМ",
+              "text": "именительный"
+            },
+            {
+              "ident": "РОД",
+              "text": "родительный"
+            },
+            {
+              "ident": "ПАРТ",
+              "text": "партитивный"
+            },
+            {
+              "ident": "ДАТ",
+              "text": "дательный"
+            },
+            {
+              "ident": "ВИН",
+              "text": "винительный"
+            },
+            {
+              "ident": "ТВОР",
+              "text": "творительный"
+            },
+            {
+              "ident": "ПР",
+              "text": "предложный"
+            },
+            {
+              "ident": "МЕСТН",
+              "text": "местный"
+            },
+            {
+              "ident": "ЗВ",
+              "text": "звательный"
+            }
+          ]
+        },
+        {
+          "dbfield": "additional",
+          "text": "дополнительные характеристики",
+          "values": [
+            {
+              "ident": "СЛ",
+              "text": "словосложение"
+            },
+            {
+              "ident": "СМЯГ",
+              "text": "смягченная степень"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "partOfSpeech": "A",
+      "text": "прилагательное",
+      "properties": [
+        {
+          "dbfield": "animacy",
+          "text": "одушевленность",
+          "values": [
+            {
+              "ident": "ОД",
+              "text": "одушевленное"
+            },
+            {
+              "ident": "НЕОД",
+              "text": "неодушевленное"
+            }
+          ]
+        },
+        {
+          "dbfield": "gender",
+          "text": "род",
+          "values": [
+            {
+              "ident": "МУЖ",
+              "text": "мужской"
+            },
+            {
+              "ident": "ЖЕН",
+              "text": "женский"
+            },
+            {
+              "ident": "СРЕД",
+              "text": "женский"
+            }
+          ]
+        },
+        {
+          "dbfield": "count",
+          "text": "число",
+          "values": [
+            {
+              "ident": "ЕД",
+              "text": "единственное"
+            },
+            {
+              "ident": "МН",
+              "text": "множественное"
+            }
+          ]
+        },
+        {
+          "dbfield": "case",
+          "text": "падеж",
+          "values": [
+            {
+              "ident": "ИМ",
+              "text": "именительный"
+            },
+            {
+              "ident": "РОД",
+              "text": "родительный"
+            },
+            {
+              "ident": "ПАРТ",
+              "text": "партитивный"
+            },
+            {
+              "ident": "ДАТ",
+              "text": "дательный"
+            },
+            {
+              "ident": "ВИН",
+              "text": "винительный"
+            },
+            {
+              "ident": "ТВОР",
+              "text": "творительный"
+            },
+            {
+              "ident": "ПР",
+              "text": "предложный"
+            },
+            {
+              "ident": "МЕСТН",
+              "text": "местный"
+            },
+            {
+              "ident": "ЗВ",
+              "text": "звательный"
+            }
+          ]
+        },
+        {
+          "dbfield": "comparison",
+          "text": "степень сравнения",
+          "values": [
+            {
+              "ident": "СРАВ",
+              "text": "сравнительная"
+            },
+            {
+              "ident": "ПРЕВ",
+              "text": "превосходная"
+            },
+            {
+              "ident": "NULL",
+              "text": "положительная"
+            }
+          ]
+        },
+        {
+          "dbfield": "short",
+          "text": "краткость",
+          "values": [
+            {
+              "ident": "КР",
+              "text": "краткая"
+            },
+            {
+              "ident": "NULL",
+              "text": "полная"
+            }
+          ]
+        },
+        {
+          "dbfield": "additional",
+          "text": "дополнительные характеристики",
+          "values": [
+            {
+              "ident": "СЛ",
+              "text": "словосложение"
+            },
+            {
+              "ident": "СМЯГ",
+              "text": "смягченная степень"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "partOfSpeech": "V",
+      "text": "глагол",
+      "properties": [
+        {
+          "dbfield": "animacy",
+          "text": "одушевленность",
+          "values": [
+            {
+              "ident": "ОД",
+              "text": "одушевленное"
+            },
+            {
+              "ident": "НЕОД",
+              "text": "неодушевленное"
+            }
+          ]
+        },
+        {
+          "dbfield": "gender",
+          "text": "род",
+          "values": [
+            {
+              "ident": "МУЖ",
+              "text": "мужской"
+            },
+            {
+              "ident": "ЖЕН",
+              "text": "женский"
+            },
+            {
+              "ident": "СРЕД",
+              "text": "женский"
+            }
+          ]
+        },
+        {
+          "dbfield": "count",
+          "text": "число",
+          "values": [
+            {
+              "ident": "ЕД",
+              "text": "единственное"
+            },
+            {
+              "ident": "МН",
+              "text": "множественное"
+            }
+          ]
+        },
+        {
+          "dbfield": "case",
+          "text": "падеж",
+          "values": [
+            {
+              "ident": "ИМ",
+              "text": "именительный"
+            },
+            {
+              "ident": "РОД",
+              "text": "родительный"
+            },
+            {
+              "ident": "ПАРТ",
+              "text": "партитивный"
+            },
+            {
+              "ident": "ДАТ",
+              "text": "дательный"
+            },
+            {
+              "ident": "ВИН",
+              "text": "винительный"
+            },
+            {
+              "ident": "ТВОР",
+              "text": "творительный"
+            },
+            {
+              "ident": "ПР",
+              "text": "предложный"
+            },
+            {
+              "ident": "МЕСТН",
+              "text": "местный"
+            },
+            {
+              "ident": "ЗВ",
+              "text": "звательный"
+            }
+          ]
+        },
+        {
+          "dbfield": "short",
+          "text": "краткость",
+          "values": [
+            {
+              "ident": "КР",
+              "text": "краткая"
+            },
+            {
+              "ident": "NULL",
+              "text": "полная"
+            }
+          ]
+        },
+        {
+          "dbfield": "form",
+          "text": "форма глагола",
+          "values": [
+            {
+              "ident": "ИНФ",
+              "text": "инфинитив"
+            },
+            {
+              "ident": "ПРИЧ",
+              "text": "причастие"
+            },
+            {
+              "ident": "ДЕЕПР",
+              "text": "деепричастие"
+            },
+            {
+              "ident": "NULL",
+              "text": "личная форма"
+            }
+          ]
+        },
+        {
+          "dbfield": "mood",
+          "text": "наклонение",
+          "values": [
+            {
+              "ident": "ИЗЪЯВ",
+              "text": "изъявительное"
+            },
+            {
+              "ident": "ПОВ",
+              "text": "повелительное"
+            }
+          ]
+        },
+        {
+          "dbfield": "aspect",
+          "text": "вид",
+          "values": [
+            {
+              "ident": "НЕСОВ",
+              "text": "несоверщенный вид"
+            },
+            {
+              "ident": "СОВ",
+              "text": "совершенный вид"
+            }
+          ]
+        },
+        {
+          "dbfield": "tense",
+          "text": "время",
+          "values": [
+            {
+              "ident": "НЕПРОШ",
+              "text": "непрошедшее"
+            },
+            {
+              "ident": "ПРОШ",
+              "text": "прошедшее"
+            },
+            {
+              "ident": "НАСТ",
+              "text": "настоящее"
+            }
+          ]
+        },
+        {
+          "dbfield": "person",
+          "text": "лицо",
+          "values": [
+            {
+              "ident": "1-Л",
+              "text": "первое лицо"
+            },
+            {
+              "ident": "2-Л",
+              "text": "второе лицо"
+            },
+            {
+              "ident": "3-Л",
+              "text": "третье лицо"
+            }
+          ]
+        },
+        {
+          "dbfield": "pledge",
+          "text": "залог",
+          "values": [
+            {
+              "ident": "страд",
+              "text": "страдательный"
+            },
+            {
+              "ident": "NULL",
+              "text": "действительный"
+            }
+          ]
+        }
+
+      ]
+    },
+    {
+      "partOfSpeech": "ADV",
+      "text": "наречие",
+      "properties": [
+        {
+          "dbfield": "comparison",
+          "text": "степень сравнения",
+          "values": [
+            {
+              "ident": "СРАВ",
+              "text": "сравнительная"
+            },
+            {
+              "ident": "ПРЕВ",
+              "text": "превосходная"
+            },
+            {
+              "ident": "NULL",
+              "text": "положительная"
+            }
+
+          ]
+        },
+        {
+          "dbfield": "additional",
+          "text": "дополнительные характеристики",
+          "values": [
+            {
+              "ident": "СЛ",
+              "text": "словосложение"
+            },
+            {
+              "ident": "СМЯГ",
+              "text": "смягченная степень"
+            }
+          ]
+        }
+
+      ]
+    },
+    {
+      "partOfSpeech": "NUM",
+      "text": "числительное",
+      "properties": [
+        {
+          "dbfield": "animacy",
+          "text": "одушевленность",
+          "values": [
+            {
+              "ident": "ОД",
+              "text": "одушевленное"
+            },
+            {
+              "ident": "НЕОД",
+              "text": "неодушевленное"
+            }
+          ]
+        },
+        {
+          "dbfield": "gender",
+          "text": "род",
+          "values": [
+            {
+              "ident": "МУЖ",
+              "text": "мужской"
+            },
+            {
+              "ident": "ЖЕН",
+              "text": "женский"
+            },
+            {
+              "ident": "СРЕД",
+              "text": "женский"
+            }
+          ]
+        },
+        {
+          "dbfield": "count",
+          "text": "число",
+          "values": [
+            {
+              "ident": "ЕД",
+              "text": "единственное"
+            },
+            {
+              "ident": "МН",
+              "text": "множественное"
+            }
+          ]
+        },
+        {
+          "dbfield": "case",
+          "text": "падеж",
+          "values": [
+            {
+              "ident": "ИМ",
+              "text": "именительный"
+            },
+            {
+              "ident": "РОД",
+              "text": "родительный"
+            },
+            {
+              "ident": "ПАРТ",
+              "text": "партитивный"
+            },
+            {
+              "ident": "ДАТ",
+              "text": "дательный"
+            },
+            {
+              "ident": "ВИН",
+              "text": "винительный"
+            },
+            {
+              "ident": "ТВОР",
+              "text": "творительный"
+            },
+            {
+              "ident": "ПР",
+              "text": "предложный"
+            },
+            {
+              "ident": "МЕСТН",
+              "text": "местный"
+            },
+            {
+              "ident": "ЗВ",
+              "text": "звательный"
+            }
+          ]
+        },
+        {
+          "dbfield": "additional",
+          "text": "дополнительные характеристики",
+          "values": [
+            {
+              "ident": "СЛ",
+              "text": "словосложение"
+            },
+            {
+              "ident": "СМЯГ",
+              "text": "смягченная степень"
+            }
+          ]
+        }
+
+      ]
+    },
+    {
+      "partOfSpeech": "PR",
+      "text": "предлог",
+      "properties": []
+    },
+    {
+      "partOfSpeech": "COM",
+      "text": "композит",
+      "properties": [
+        {
+          "dbfield": "additional",
+          "text": "дополнительные характеристики",
+          "values": [
+            {
+              "ident": "СЛ",
+              "text": "словосложение"
+            },
+            {
+              "ident": "СМЯГ",
+              "text": "смягченная степень"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "partOfSpeech": "CONJ",
+      "text": "союз",
+      "properties": []
+    },
+    {
+      "partOfSpeech": "PART",
+      "text": "частица",
+      "properties": []
+    },
+    {
+      "partOfSpeech": "P",
+      "text": "слово-предложение",
+      "properties": []
+    },
+    {
+      "partOfSpeech": "INTJ",
+      "text": "междометие",
+      "properties": []
+    },
+    {
+      "partOfSpeech": "NID",
+      "text": "иноязычное вкрапление",
+      "properties": [
+        {
+          "dbfield": "animacy",
+          "text": "одушевленность",
+          "values": [
+            {
+              "ident": "ОД",
+              "text": "одушевленное"
+            },
+            {
+              "ident": "НЕОД",
+              "text": "неодушевленное"
+            }
+          ]
+        },
+        {
+          "dbfield": "gender",
+          "text": "род",
+          "values": [
+            {
+              "ident": "МУЖ",
+              "text": "мужской"
+            },
+            {
+              "ident": "ЖЕН",
+              "text": "женский"
+            },
+            {
+              "ident": "СРЕД",
+              "text": "женский"
+            }
+          ]
+        },
+        {
+          "dbfield": "count",
+          "text": "число",
+          "values": [
+            {
+              "ident": "ЕД",
+              "text": "единственное"
+            },
+            {
+              "ident": "МН",
+              "text": "множественное"
+            }
+          ]
+        },
+        {
+          "dbfield": "case",
+          "text": "падеж",
+          "values": [
+            {
+              "ident": "ИМ",
+              "text": "именительный"
+            },
+            {
+              "ident": "РОД",
+              "text": "родительный"
+            },
+            {
+              "ident": "ПАРТ",
+              "text": "партитивный"
+            },
+            {
+              "ident": "ДАТ",
+              "text": "дательный"
+            },
+            {
+              "ident": "ВИН",
+              "text": "винительный"
+            },
+            {
+              "ident": "ТВОР",
+              "text": "творительный"
+            },
+            {
+              "ident": "ПР",
+              "text": "предложный"
+            },
+            {
+              "ident": "МЕСТН",
+              "text": "местный"
+            },
+            {
+              "ident": "ЗВ",
+              "text": "звательный"
+            }
+          ]
+        }
+      ]
+    }
+  ];
 
   $scope.stats = "befload";
 
@@ -814,6 +1561,8 @@ function TabManagerCtrl($scope, ajaxService) {
       i++;
     });
     $scope.tabManager.tabItems = items;
+
+    $scope.sendData();
   }
 
   $scope.tabManager.getLabelClass = function(tab, index) {
@@ -855,7 +1604,7 @@ function TabManagerCtrl($scope, ajaxService) {
 
   $scope.tabManagerDep.selectItems = [];
 
-  angular.forEach($scope.posData, function(pos) {
+  angular.forEach($scope.posDataDep, function(pos) {
     $scope.tabManagerDep.selectItems.push(pos);
   });
 
@@ -867,7 +1616,7 @@ function TabManagerCtrl($scope, ajaxService) {
   $scope.tabManagerDep.addTab = function(){
     if($scope.selectedPosDep != undefined) {
       $scope.tabManagerDep.selectItems = [];
-      angular.forEach($scope.posData, function(pos) {
+      angular.forEach($scope.posDataDep, function(pos) {
         if(pos.partOfSpeech == $scope.selectedPosDep) {
           $scope.tabManagerDep.tabItems.push({
             partOfSpeech: pos.partOfSpeech,
@@ -891,6 +1640,8 @@ function TabManagerCtrl($scope, ajaxService) {
       i++;
     });
     $scope.tabManagerDep.tabItems = items;
+
+    $scope.sendData();
   }
 
   $scope.tabManagerDep.getLabelClass = function(tab, index) {
